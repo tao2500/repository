12959584e7b7dc8fd6898f01254aa31c90daf06c
刘金涛 <tao2500594037@qq.com>
2021-11-05 13:22:58 +0800

    testthree

--- IParams.java
+++ IParams.java
@@ -8,5 +8,5 @@ interface IParams{
 	public int getSmall();
 	/* 获取停车序列，例如 [1 2 2 3] 表示 依次停一辆大车，中车，中车，小车*/
 	public ArrayList<Integer> getPlanParking();
-	public int getSize();
+	public int getSize();  
 }
